ce19f7d5b9250ceaf499c418f970fa8dbeb45c05
sam <sam@sam-appstesting2>
2021-06-21 12:44:41 +0700

    login2-2-update registration port

--- src/java/controller/ValidateUser.java
+++ src/java/controller/ValidateUser.java
@@ -102,7 +102,8 @@ public class ValidateUser extends HttpServlet {
                          
                                     //response.sendRedirect("http://localhost:8080/BookLandMain/MainPage.jsp");
                                     //response.sendRedirect("http://appsmainpage:9002/BookLandMain/MainPage.jsp");
-                                    response.sendRedirect("http://jenkins.fitraalim.com:9012/BookLandMain/MainPage.jsp");
+                                    //response.sendRedirect("http://jenkins.fitraalim.com:9012/BookLandMain/MainPage.jsp");
+                                    response.sendRedirect("http://jenkins.fitraalim.com:9002/BookLandMain/MainPage.jsp");
                                     
                                 }else {
                                     request.setAttribute("error", "invalid username/password");
